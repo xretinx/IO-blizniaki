b23b5dfef4421b7c062367d14cda26eff8b11c2e
Your Name <you@example.com>
2024-01-24 21:53:31 +0100

    .

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -4,17 +4,124 @@ document.addEventListener("DOMContentLoaded", function () {
 
   form.addEventListener("submit", function (e) {
     e.preventDefault();
-    const formData = new FormData(form);
 
-    fetch("/find_dog_breed", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        resultDiv.innerHTML = `Most similar dog breed: ${data.dog_breed}`;
-      });
+    // Animacja znikania diva o klasie "form-group-wider"
+    const formGroupWider = document.querySelector(".form-group-wider");
+    formGroupWider.style.transition = "opacity 1s";
+    formGroupWider.style.opacity = 0;
+
+    const formGroupThinner = document.querySelector(".form-group-thinner");
+    formGroupThinner.style.transition = "opacity 1s";
+    formGroupThinner.style.opacity = 0;
+
+    // Animacja znikania przycisków z klasą "custom-button"
+    const customButtons = document.querySelectorAll(".custom-button");
+    customButtons.forEach((button) => {
+      button.style.transition = "opacity 1s";
+      button.style.opacity = 0;
+    });
+
+
+
+   
+
+    // Poczekaj 1 sekundę przed usunięciem zawartości diva
+    setTimeout(function () {
+      // Usuń zawartość diva o klasie "form-group-wider"
+      formGroupWider.innerHTML = `<div class="loader">
+      <span style="--i:1;" class="fa-solid fa-bone"></span>
+      <span style="--i:2;" class="fa-solid fa-bone"></span>
+      <span style="--i:3;" class="fa-solid fa-bone"></span>
+      <span style="--i:4;" class="fa-solid fa-bone"></span>
+      <span style="--i:5;" class="fa-solid fa-bone"></span>
+      <span style="--i:6;" class="fa-solid fa-bone"></span>
+      <span style="--i:7;" class="fa-solid fa-bone"></span>
+      <span style="--i:8;" class="fa-solid fa-bone"></span>
+      <span style="--i:9;" class="fa-solid fa-bone"></span>
+      <span style="--i:10;" class="fa-solid fa-bone"></span>
+      <span style="--i:11;" class="fa-solid fa-bone"></span>
+      <span style="--i:12;" class="fa-solid fa-bone"></span>
+      <span style="--i:13;" class="fa-solid fa-bone"></span>
+      <span style="--i:14;" class="fa-solid fa-bone"></span>
+      <span style="--i:15;" class="fa-solid fa-bone"></span>
+      <span style="--i:16;" class="fa-solid fa-bone"></span>
+      <span style="--i:17;" class="fa-solid fa-bone"></span>
+      <span style="--i:18;" class="fa-solid fa-bone"></span>
+      <span style="--i:19;" class="fa-solid fa-bone"></span>
+      <span style="--i:20;" class="fa-solid fa-bone"></span>
+  
+    
+  </div>`;
+
+      // Wstaw zdjęcie uploaded-image
+      //const uploadedImage = document.getElementById("uploaded-image");
+      //formGroupWider.appendChild(uploadedImage);
+
+      // Przywróć widoczność diva o klasie "form-group-wider"
+      formGroupWider.style.opacity = 1;
+
+
+      setTimeout(function (){
+
+
+        const formGroupWider = document.querySelector(".form-group-wider");
+        formGroupWider.style.transition = "opacity 1s";
+        formGroupWider.style.opacity = 0;
+        formGroupWider.innerHTML ="";
+        
+      },3800)
+
+
+      setTimeout(function (){
+        formGroupWider.style.transition = "opacity 1s";
+        formGroupWider.style.opacity = 1;
+        const uploadedImage = document.getElementById("uploaded-image");
+        formGroupWider.appendChild(uploadedImage);
+        uploadedImage.style.transform = "scale(3)";
+       
+      },4100)
+
+    
+      setTimeout(function (){
+      const formGroupWider = document.querySelector(".form-group-wider");
+      formGroupWider.style.transition = "opacity 1s";
+      formGroupWider.style.opacity = 0;
+      formGroupWider.innerHTML ="";
+      
+    },4700)
+
+    setTimeout(function () {
+      const formData = new FormData(form);
+
+      fetch("/find_dog_breed", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          resultDiv.innerHTML = `<i class="heading"> Most similar dog breed: ${data.dog_breed}</i>`;
+
+        formGroupWider.style.transition = "opacity 1s";
+        formGroupWider.style.opacity = 1;
+        
+        formGroupWider.innerHTML = '<img src="../static/images/Baza_reprezentacja/' + data.dog_breed + '.jpg" alt="Your Image">';
+
+       
+
+
+        });
+    }, 5000);
+
+ 
+
+
+
+
+      // Poczekaj 1 sekundę przed wysłaniem formularza
+      
+    }, 300);
   });
+
   const fileInput = document.getElementById("file-input");
   const fileNameLabel = document.getElementById("file-name-label");
   const uploadedImage = document.getElementById("uploaded-image");
